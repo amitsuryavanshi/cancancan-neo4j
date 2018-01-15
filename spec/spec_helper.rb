f4f5a63522eedfdc56340f740b5233be2d1ed511
amitsuryavanshi <amitbsuryavanshi@gmail.com>
2018-01-15 20:49:58 +0530

    unit specs for adaptor

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,36 +1,27 @@
-$LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
-require 'cancancan/neo4j'
-Dir["#{__dir__}/support/*.rb"].each { |f| require f }
+require 'rubygems'
+require 'bundler/setup'
 
+Bundler.require
+
+require 'matchers'
 require 'cancan/matchers'
 
-RSpec.configure do |config|
-  config.expect_with :rspec do |expectations|
-    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
-  end
-
-  config.mock_with :rspec do |mocks|
-    mocks.verify_partial_doubles = true
-  end
-
-  config.filter_run :focus
-  config.run_all_when_everything_filtered = true
-
-  config.example_status_persistence_file_path = 'spec/examples.txt'
-
-  config.disable_monkey_patching!
-
-  config.warnings = true
-
-  config.default_formatter = 'doc' if config.files_to_run.one?
-
-  config.profile_examples = 10
-
-  config.order = :random
-
-  Kernel.srand config.seed
+# I8n setting to fix deprecation.
+if defined?(I18n) && I18n.respond_to?('enforce_available_locales=')
+  I18n.enforce_available_locales = false
 end
 
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each do |f|
-  require f
+# Add support to load paths
+$LOAD_PATH.unshift File.expand_path('../support', __FILE__)
+Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each { |f| require f }
+
+RSpec.configure do |config|
+  config.filter_run focus: true
+  config.run_all_when_everything_filtered = true
+  config.mock_with :rspec
+  config.order = 'random'
+
+  config.expect_with :rspec do |c|
+    c.syntax = :expect
+  end
 end
--- spec/support/models.rb
+++ spec/support/models.rb
@@ -1,1 +1,48 @@
+class Project
+  include Neo4j::ActiveNode
+  property :name, type: String
+  # property :content, type: String
 
+  # has_one :out, :author, type: :auther, model_class: :User
+end
+
+class Category
+  include Neo4j::ActiveNode
+  property :name, type: String
+  property :visible, type: Boolean
+
+  has_many :in, :articles, origin: :category
+end
+
+class Article
+  include Neo4j::ActiveNode
+  property :name, type: String
+  property :published, type: Boolean
+  property :secret, type: Boolean
+  property :priority, type: Integer
+
+  has_one :out, :category, type: :category
+  has_many :in, :comments, origin: :article
+  has_many :out, :mentions, type: :mention, model_class: :Mention
+  has_one :out, :user, type: :user
+end
+
+class Mention
+  include Neo4j::ActiveNode
+  has_one :in, :article, type: :mention
+  has_one :out, :user, type: :user
+end
+
+class Comment
+  include Neo4j::ActiveNode
+  property :spam, type: Boolean
+  
+  has_one :out, :article, type: :article
+end
+
+class User
+  include Neo4j::ActiveNode
+
+  has_many :in, :articles, origin: :user
+  has_many :in, :mentions, origin: :user
+end
